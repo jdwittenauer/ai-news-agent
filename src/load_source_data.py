0369538202a74aba7ed6015e74a2f038d26330a2
John Wittenauer <jdwittenauer@gmail.com>
2025-07-11 22:08:12 -0400

    more supabase work

---
+++ src/load_source_data.py
@@ -0,0 +1,61 @@
+import argparse
+import feedparser
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+from supabase import create_client
+
+
+def get_published_date(published_str):
+    date_obj = datetime.strptime(published_str[:16], "%a, %d %b %Y")
+    return str(date_obj.date())
+
+def load_recent_entries(feed, supabase, num_entries):
+    print(f"Reading entries from {feed.feed.title}...")
+    i = 0
+    for entry in feed.entries[:num_entries]:
+        if entry.get("content", "None") != "None":
+            _ = (
+                supabase.table("ai_news_raw_daily")
+                .insert([
+                    {
+                        "created_at": str(datetime.now()),
+                        "title": entry.title,
+                        "link": entry.link,
+                        "published_date": get_published_date(entry.published),
+                        "summary": entry.summary,
+                        "content": entry.content[0]['value']
+                    }
+                ])
+                .execute()
+            )
+            i += 1
+    print(f"Loaded {i} rows to the database.")
+
+def clear_table(supabase):
+    response = (
+        supabase.table("ai_news_raw_daily")
+        .delete()
+        .neq("id", 0)
+        .execute()
+    )
+    print(f"Deleted {len(response.data)} rows from the database.")
+
+def main():
+    load_dotenv()
+    ainews_url = "https://news.smol.ai/rss.xml"
+    db_url = os.getenv("SUPABASE_LOCAL_API_URL")
+    db_key = os.getenv("SUPABASE_LOCAL_SERVICE_KEY")
+    feed = feedparser.parse(ainews_url)
+    supabase = create_client(db_url, db_key)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--clear", action="store_true", help="Clear the table before loading new entries")
+    parser.add_argument("-n", "--num_entries", type=int, default=10, help="Number of recent entries to load")
+    args = parser.parse_args()
+    if args.clear:
+        clear_table(supabase)
+    load_recent_entries(feed, supabase, args.num_entries)
+
+
+if __name__ == "__main__":
+    main()
--- supabase/README.md
+++ supabase/README.md
@@ -3,10 +3,33 @@
 ## Helpful Commands
 
 Initialize project:
-> npx supabase init
+<pre>supabase init</pre>
 
 Start the stack:
-> npx supabase start
+<pre>supabase start</pre>
 
 Shut down the stack:
-> npx supabase stop
+<pre>
+supabase stop
+docker ps --filter name=supabase -q | xargs docker stop
+</pre>
+
+Create initial migration file:
+<pre>supabase db dump --local --file supabase/migrations/$(date +%Y%m%d%H%M%S)_init.sql</pre>
+
+Create schema diff migration file:
+<pre>supabase db diff --local --file supabase/migrations/$(date +%Y%m%d%H%M%S)_update.sql</pre>
+
+Rebuild the database:
+<pre>supabase db reset</pre>
+
+## Project Services
+
+<pre>
+         API URL: http://127.0.0.1:54321
+     GraphQL URL: http://127.0.0.1:54321/graphql/v1
+  S3 Storage URL: http://127.0.0.1:54321/storage/v1/s3
+          DB URL: postgresql://postgres:postgres@127.0.0.1:54322/postgres
+      Studio URL: http://127.0.0.1:54323
+    Inbucket URL: http://127.0.0.1:54324
+</pre>
